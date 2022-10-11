f39480af224dcf101f33e5d87a55650d28d512f7
Ilja <ilja.szegedi@gmail.com>
2022-10-11 11:30:06 +0200

    Copied override code

--- DZC10 Group 11 Puzzle Platformer/Assets/Scripts/PlayerInverseGravity.cs
+++ DZC10 Group 11 Puzzle Platformer/Assets/Scripts/PlayerInverseGravity.cs
@@ -26,5 +26,46 @@ public class PlayerInverseGravity : Player
         jumpSpeed = -jumpSpeed;
     }
 
-    // TODOa: override Player.Move() to allow for a different jump check
+    // TODO: override Player.Move() to allow for a different jump check
+    new public void Move()
+    {
+        // Horizontal movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * moveSpeed;
+
+        // Jumping and falling
+        if (controller.isGrounded == true)
+        {
+            // Jump when pressing space/w/arrow up by setting vetical speed
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+        else // Player is in the air
+        {
+            // This checks if the character is not moving but still has vertical velocity
+            // Without it, the player doesn't stop their jump when bumping their head
+            if (controller.velocity.y == 0.0f && yVelocity > 0.0f)
+            {
+                yVelocity = 0f;
+            }
+
+            // Applying gravity
+            yVelocity -= gravity * Time.deltaTime;
+        }
+
+        // Sets a maximal falling speed
+        if (yVelocity <= terminalVelocity)
+        {
+            yVelocity = terminalVelocity;
+        }
+
+        // Apply changes to vertical speed
+        velocity.y = yVelocity;
+
+        // Apply horizontal and vertical movement changes made this frame
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
