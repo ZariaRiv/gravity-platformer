bc276d437db961492c448d6189d91091a78dcdd6
Ilja <ilja.szegedi@gmail.com>
2022-10-11 12:54:36 +0200

    Stashed original plan for InvertedGravity

--- DZC10 Group 11 Puzzle Platformer/Assets/Scripts/PlayerInverseGravity.cs
+++ DZC10 Group 11 Puzzle Platformer/Assets/Scripts/PlayerInverseGravity.cs
@@ -21,44 +21,7 @@ public class PlayerInverseGravity : Player
             Debug.Log("LevelManager not found!");
         }
 
-        // Inverts the gravity and jumps
-        gravity = -gravity;
-        jumpSpeed = -jumpSpeed;
-        terminalVelocity = -terminalVelocity;
-    }
-
-    // TODO: override Player.Move() to allow for a different jump check
-    new public void Move()
-    {
-        // Horizontal movement
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horizontalInput, 0, 0);
-        Vector3 velocity = direction * moveSpeed;
-
-        // Jumping and falling
-        if (controller.velocity.y < 1f)
-        {
-            // Jump when pressing space/w/arrow up by setting vetical speed
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                yVelocity = jumpSpeed;
-            }
-        }
-        else // Player is in the air
-        {
-            yVelocity -= gravity * Time.deltaTime;
-        }
-
-        // Sets a maximal falling speed
-        if (yVelocity >= terminalVelocity)
-        {
-            yVelocity = terminalVelocity;
-        }
-
-        // Apply changes to vertical speed
-        velocity.y = yVelocity;
-
-        // Apply horizontal and vertical movement changes made this frame
-        controller.Move(velocity * Time.deltaTime);
+        // Flip the player and camera
+        
     }
 }
---
+++ DZC10 Group 11 Puzzle Platformer/Assets/Scripts/PlayerInverseGravityBackup.cs
@@ -0,0 +1,66 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInverseGravity : Player
+{
+    // Override of Player.Start() to invert gravity
+    new public void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // Checks if the current scene contains a StateManager 
+        if (stateManager == null)
+        {
+            Debug.Log("StateManager not found!");
+        }
+
+        // Checks if the current scene contains a LevelManager
+        if (levelManager == null)
+        {
+            Debug.Log("LevelManager not found!");
+        }
+
+        // Inverts the gravity and jumps
+        gravity = -gravity;
+        jumpSpeed = -jumpSpeed;
+        terminalVelocity = -terminalVelocity;
+    }
+
+    // TODO: override Player.Move() to allow for a different jump check
+    new public void Move()
+    {
+        // Horizontal movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * moveSpeed;
+
+        // Jumping and falling
+        if (controller.velocity.y < 1f)
+        {
+            // Jump when pressing space/w/arrow up by setting vetical speed
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                yVelocity = jumpSpeed;
+            }
+        }
+        else // Player is in the air
+        {
+            yVelocity -= gravity * Time.deltaTime;
+        }
+
+        // Sets a maximal falling speed
+        if (yVelocity >= terminalVelocity)
+        {
+            yVelocity = terminalVelocity;
+        }
+
+        // Apply changes to vertical speed
+        velocity.y = yVelocity;
+
+        // Apply horizontal and vertical movement changes made this frame
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
+*/
