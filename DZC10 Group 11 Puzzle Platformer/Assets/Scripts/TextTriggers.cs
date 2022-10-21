0524da7c4b19e30a3e4b0d3f90b0f1b57b3b6ed6
Ilja <ilja.szegedi@gmail.com>
2022-10-21 15:59:57 +0200

    Added triggerable text prefab

---
+++ DZC10 Group 11 Puzzle Platformer/Assets/Scripts/TextTriggers.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTriggers : MonoBehaviour
+{
+    [HideInInspector] public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = transform.GetChild(0).gameObject;
+        canvas.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canvas.SetActive(false);
+        }
+    }
+}
