45b3389818ac852dac3560ee54171aa131de5fec
Ilja <ilja.szegedi@gmail.com>
2022-10-23 17:44:01 +0200

    Reserved a scene for the lab

--- DZC10 Group 11 Puzzle Platformer/Assets/Scripts/LevelManager.cs
+++ DZC10 Group 11 Puzzle Platformer/Assets/Scripts/LevelManager.cs
@@ -7,14 +7,15 @@ public class LevelManager : MonoBehaviour
 {
     Dictionary<int, int> sceneDimensions = new Dictionary<int, int>() // <buildIndex, dimensions>
     {
-        {0, 1}, // Menu/intro scene with 1 dimension
-        {1, 2}, // Low Gravity 1 with 2 dimensions
-        {2, 2}, // Low Gravity 2 with 2 dimensions
-        {3, 3}, // Small 1 with 3 dimensions
-        {4, 3}, // Small 2 with 3 dimensions
-        {5, 4}, // Inverse Gravity 1 with 4 dimensions
-        {6, 4}, // Inverse Gravity 2 with 4 dimensions
-        {7, 4}, // Test scene with 4 dimensions
+        {0, 1}, // Intro scene with 1 dimension
+        {1, 1}, // Lab scene with 1 dimension
+        {2, 2}, // Low Gravity 1 with 2 dimensions
+        {3, 2}, // Low Gravity 2 with 2 dimensions
+        {4, 3}, // Small 1 with 3 dimensions
+        {5, 3}, // Small 2 with 3 dimensions
+        {6, 4}, // Inverse Gravity 1 with 4 dimensions
+        {7, 4}, // Inverse Gravity 2 with 4 dimensions
+        {8, 4}, // Demo scene with 4 dimensions
     }; 
 
     public int getSceneDimensions()
